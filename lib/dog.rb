4eba5b77001eed1107058c522f8ee813f598ccfb
Manuel Eduardo Baez Diaz <baezdiazm@yahoo.com>
2020-06-18 00:10:35 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,19 +1,19 @@
 class Dog
-
-def initialize(name, breed)
-  if breed == ""
-@breed = "Mutt"
-else
-  @breed = breed
-end
-end
-
-def name=(name)
-  @name = name
-end
-
-def name(name)
-@name = name
-end
-
+  
+  def initialize(name, breed)
+    if breed == ""
+      @breed = "Mutt"
+    else
+      @breed = breed
+    end
+  end
+  
+  def name=(name)
+    @name = name
+  end
+  
+  def name(name)
+    @name = name
+  end
+  
 end
